236c48d94b6b41b9cb854d3546ac109d54c7a38f
giandrasantos <giandra.santos@aluno.senai.br>
2026-02-13 12:04:04 -0300

    aula 5 finalizada

--- game.py
+++ game.py
@@ -1,23 +1,61 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "Game: Adivinhe a imagem"
+    mensagem = ft.Text("Escolha a opção correta!", color="#fafa02", size=20)
+    resposta_correta = "Mickey"
+
+    def verificar_resposta(e):
+        if e.control.content == resposta_correta:
+            mensagem.value = "Miska, Muska, PARABÉNS, VOCÊ ARCETOU!!"
+        else:
+            mensagem.value = "Resposta errada"
+        page.update()
+
+    page.title = "Game: Adivinhe o personagem"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.bgcolor = "#660505"
 
     page.add(
         ft.Column(
             controls=[
                 ft.Text(
-                    "Adivinhe a imagem",
+                    "Adivinhe o personagem",
                     size=24,
-                    weight="bold"
+                    weight="bold",
+                    color="#fafa02"
                 ),
                 ft.Image(
-                    src="images/gatinho.jpg",
-                    height=200
-                )
-            ]
+                    src="images/mickey.jpg",
+                    height=200,
+                    border_radius=15
+                ),
+                ft.Row(
+                    controls=[
+                        ft.Button(
+                            content="Pateta",
+                            on_click=verificar_resposta,
+                            bgcolor="#fa0217",
+                            color="#fafa02"
+                        ),
+                        ft.Button(
+                            content="Mickey",
+                            on_click=verificar_resposta,
+                            bgcolor="#fa0217",
+                            color="#fafa02"
+                        ),
+                        ft.Button(
+                            content="Minnie",
+                            on_click=verificar_resposta,
+                            bgcolor="#fa0217",
+                            color="#fafa02"
+                        ),
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER
+                ),
+                mensagem
+            ],
+            horizontal_alignment = ft.CrossAxisAlignment.CENTER
         )
     )
 
